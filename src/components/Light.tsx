54772526b30f3a0dafb1c7a92628cdcb32b4a84e
JLake310 <0310kjy@gmail.com>
2023-02-23 20:38:57 +0900

    Update Light.tsx

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -4,7 +4,7 @@ const Lights = () => {
   return (
     <>
       <ambientLight intensity={0.1} />
-      <directionalLight position={[40, 10, 5]} intensity={0.2} />
+      <directionalLight position={[0, 0, 10]} intensity={0.1} />
       <directionalLight
         castShadow
         position={[10, 420, 100]}
@@ -17,7 +17,7 @@ const Lights = () => {
         shadow-camera-top={40}
         shadow-camera-bottom={-10}
       />
-      <spotLight intensity={0.5} position={[90, 100, 50]} castShadow />
+      <spotLight intensity={0.1} position={[90, 100, 50]} castShadow />
     </>
   );
 };
