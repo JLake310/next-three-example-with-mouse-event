0d8bdd20023271114ad5c2e13ad5f0437a8a0068
JLake310 <0310kjy@gmail.com>
2023-03-04 21:20:56 +0900

    [Feat] see all models with buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,11 @@
 import React from "react";
 import ModelViewer from "@/components/ModelViewer";
 
-const MBTIs = [
-  { name: "ENFJ" },
-  { name: "ENFP" },
-  { name: "ENTJ" },
-  { name: "ENTP" },
-  { name: "ESFJ" },
-  { name: "ESFP" },
-  { name: "ESTJ" },
-  { name: "ESTP" },
-  { name: "INFJ" },
-  { name: "INFP" },
-  { name: "INTJ" },
-  { name: "ISFJ" },
-  { name: "ISTJ" },
-  { name: "ISFP" },
-  { name: "INTP" },
-  { name: "ISTP" },
-];
-
 const App = () => {
   return (
     <>
       <div className="wrapper">
-        {MBTIs.map((MBTI) => (
-          <>
-            <ModelViewer mbti={MBTI.name} />
-            <span>{MBTI.name}</span>
-          </>
-        ))}
+        <ModelViewer />
       </div>
     </>
   );
